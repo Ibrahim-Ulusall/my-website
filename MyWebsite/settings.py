831927759476650f3022bb15a8da47462b5efa54
ibrahim <ibrahimulusal185@gmail.com>
2023-06-09 01:20:26 +0300

    setting.py updated

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
