d989a11beb9f969fba1a3081685828819287a3a5
ibrahim <ibrahimulusal185@gmail.com>
2023-06-10 20:37:24 +0300

    Bug fixed

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w-fj1v5-2o6#e#xworn%vps)m)_s@s2z(r8dwj7t(wek-td)kn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['byte-genius.herokuapp.com','127.0.0.1']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['byte-genius.herokuapp.com','127.0.0.1']
+
 
 # Application definition
 
